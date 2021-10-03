06bcfb7911a5cd79de477adef6f59c889a005d72
Kurja <Kurja0403@gmail.com>
2021-10-04 06:20:03 +0800

    点击标签可跳转编辑页面

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+type RecordItem = {
+    tags:string[]
+    notes:string
+    type:string
+    amount:number //数据类型 object | string
+    createdAT?:Date | undefined //类 / 构造函数
+}
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,24 +14,22 @@
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component, Watch} from 'vue-property-decorator'
+  import {Component, Watch} from 'vue-property-decorator';
+  import recordListModel from '@/models/recordListModel';
+  import tagListModel from '@/models/tagListModel'
 
-  const recordList:Record[]= JSON.parse(window.localStorage.getItem('recordList') || '[]');
 
-  type Record = {
-    tags:string[]
-    notes:string
-    type:string
-    amount:number //数据类型 object | string
-    createdAT?:Date | undefined //类 / 构造函数
-  }
+  const recordList = recordListModel.fetch();
+  const tagList = tagListModel.fetch();
+
+
   @Component({
     components: {Tags, Notes, Types, NumberPad}
   })
   export default class Money extends Vue {
-    tags = ['衣','食','住','行','彩票'];
-    recordList:Record[]= recordList ;
-    record: Record ={
+    tags = tagList;
+    recordList:RecordItem[]= recordList ;
+    record: RecordItem ={
       tags:[],notes:'',type:'-',amount:0
     };
     onUpdateTags(value: string[]){
@@ -45,14 +43,14 @@
     // }
 
     saveRecord(){
-      const record2: Record = JSON.parse(JSON.stringify(this.record));
+      const record2: RecordItem = recordListModel.clone(this.record);
       record2.createdAT = new Date();
       this.recordList.push(record2);
     }
 
     @Watch('recordList')
     onRecordListChange(){
-    window.localStorage.setItem('recordList',JSON.stringify(this.recordList));
+      recordListModel.save(this.recordList);
     }
   }
 </script>
