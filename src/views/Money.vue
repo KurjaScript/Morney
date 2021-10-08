75506e9277a040874771c03019e6ce5d26df998a
Kurja <Kurja0403@gmail.com>
2021-10-09 01:27:56 +0800

    解决id生成问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
 <!--    <Types :value="record.type" @update:value="onUpdateType"/>-->
     <Types :value.sync="record.type"/>
