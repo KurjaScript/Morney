2766d451be77cb85d2d2d5d95eb3f6052e5152bf
Kurja <kurja0403@gmail.com>
2021-10-13 21:31:45 +0800

    修复刷新变成404的bug

--- src/store/tagStore.ts
+++ src/store/tagStore.ts
@@ -6,38 +6,10 @@ const tagStore = {
 
 
 
-    removeTag(id:string){
-       let index = -1;
-       for(let i=0; this.tagList.length; i++ ){
-           if(this.tagList[i].id === id){
-                index = i;
-                break;
-           }
-       }
-       console.log(index)
-       this.tagList.splice(index,1);
-       this.saveTags();
-       return true;
-    },
-    updateTag(id:string,name:string){
-         const idList = this.tagList.map(item => item.id);
-         if(idList.indexOf(id)>=0){
-            const names = this.tagList.map(item => item.name)
-            if(names.indexOf(name) >= 0){
-                return 'duplicated';
-            }else{
-                const tag = this.tagList.filter(item => item.id === id)[0];
-                tag.name = name;
-                this.saveTags();
-                return 'success';
-            }
-         }else{
-            return 'not found';
-         }
-    },
+
 
 }
 
-tagStore.fetchTags();
+
 
 export default tagStore;
