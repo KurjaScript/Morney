40d9570b413771bf4c367be6aab7d10818fb3fd0
Kurja <kurja0403@gmail.com>
2021-10-14 23:57:06 +0800

    构建Statistics失败

---
+++ src/components/Tabs.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul class="tabs">
+    <li v-for="item in dataSource" :key="item.value"
+        :class="liClass(item)"
+        @click="selected(item)">
+      {{item.text}}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component,Prop} from 'vue-property-decorator';
+
+  type DataSourceItem = {text: string,value:string}
+
+  @Component
+  export default class Tabs extends Vue{
+    @Prop({required:true, type:Array})
+    dataSource!:DataSourceItem[];
+    @Prop(String)
+    classPrefix?:string;
+
+    liClass(item:DataSourceItem){
+      return {
+        [THIS.ClassPrefix + '-tabs-item']:this.classPrefix,
+        selected:item.value === this.valueOf()
+      };
+    }
+
+    select(item:DataSourceItem){
+      this.$emit('updata:value',item.value);
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tabs{
+    background: #C4C4C4;
+    display:flex;
+    text-align: center;
+    font-size: 24px;
+    > li{
+      width:50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position:relative;
+      &.selected::after{
+        content:'';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width:100%;
+        height: 4px;
+        background: #333;
+      }
+    }
+  }
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,20 @@
 <template>
   <Layout>
-    Statistics.vue
+    <Types class ="x"/>
   </Layout>
 </template>
 
+<style scoped lang="scss">
+  .x ::v-deep li{
+    background:
+  }
+</style>
+
 <script lang="ts">
+  import Types from '@/components/Money/Types.vue'
   export default {
     name: 'Statistics',
+    components: {Types},
   };
 </script>
 
