a5311cbe0c28f7d1f3caeead5690bcb1f3236c2a
Kurja <kurja0403@gmail.com>
2021-10-17 02:27:29 +0800

    删除Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,9 +15,8 @@
 <script lang="ts">
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
-  import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
+  import FormItem from '@/components/Money/FormItem.vue';
   import {Component,} from 'vue-property-decorator';
   import recordTypeList from '@/constants/recordTypeList';
   import Tabs from '@/components/Tabs.vue';
@@ -28,7 +27,7 @@
 
 
   @Component({
-    components: {Tabs,Tags, FormItem, Types, NumberPad},
+    components: {Tabs, Tags,FormItem, NumberPad},
   })
 
   export default class Money extends Vue {
