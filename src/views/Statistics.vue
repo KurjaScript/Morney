aca15b4706b5d1a0482d19d486af96ac560196fa
Kurja <kurja0403@gmail.com>
2021-10-18 01:32:48 +0800

    划分“今天”、“昨天”、“前天”以及其他日期

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -8,11 +8,12 @@
          :value.sync="interval"/>
 
       <ol>
-        <li v-for="(group,index) in result" :key="index">
-          <h3 class="title">{{group.title}}</h3>
+        <li v-for="(group,key) in result" :key="group.title">
+          <h3 class="title">{{beautify(group.title)}}</h3>
           <ol>
             <li v-for="item in group.items" :key="item.id"
                 class="record">
+              key2:{{item.id}}
              <span>{{tagString(item.tags)}}</span>
               <span class="notes">{{item.notes}}</span>
               <span>￥{{item.amount}} </span>
@@ -24,33 +25,17 @@
   </Layout>
 </template>
 
-<style  scoped lang="scss">
-  %item{
-    padding:8px 16px;
-    line-height: 24px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .title{
-    @extend %item;
-  }
-  .record{
-    @extend %item;
-    background: white;
-  }
-  .notes{
-    margin-right: auto;
-    margin-left: 16px;
-    color:#999
-  }
-</style>
+
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import intervalList from '@/constants/intervalList';
   import recordTypeList from '@/constants/recordTypeList';
   import Tabs from '@/components/Tabs.vue';
+  import dayjs from 'dayjs';
+
+  const  api = dayjs();
+  console.log(api)
 
 
   @Component({
@@ -60,21 +45,25 @@
     tagString(tags: Tag[]){
       return tags.length === 0? '无' : tags.join(',')
     }
+    beautify(string: string){
+      const day = dayjs(string)
+      const now = dayjs()
+      if(dayjs(string).isSame(now,'day')){
+        return '今天'
+      }else if(dayjs(string).isSame(now.subtract(1, 'day'),'day')){
+        return '昨天'
+      }else if(dayjs(string).isSame(now.subtract(2, 'day'),'day')){
+        return '前天'
+      }else if(day.isSame(now, 'year')){
+        return day.format('M月D日')
+      } else{
+        return day.format('YYYY年M月D日')
+      }
+    }
     get recordList(){
       return (this.$store.state as RootState).recordList;
     }
 
-    // get result(){
-    //   const  {recordList} = this;
-    //   type HasTableValue = {title:string, item:RecordList[]}
-    //   const hashTable:{[key: string]: HasTableValue} = {};
-    //   for(let i = 0; i < recordList.length; i++){
-    //     const [data, time] = recordList[i].createdAT!.split('T')
-    //     hashTable[data] = hashTable[data] || {title:data, item:[]}
-    //     hashTable[data].items.push(recordList[i])
-    //   }
-    //   return hashTable;
-    // }
     get result(){
       const {recordList} = this;
       type Items = RecordItem[];
@@ -83,7 +72,6 @@
       const hashTable: {[key:string]: HashTableValue} = {};
       for (let i = 0; i < recordList.length; i++){
         const [data, time] = recordList[i].createdAT!.split('T');
-        console.log(time)
         hashTable[data] = hashTable[data] || {title: data, items:[]}
         hashTable[data].items.push(recordList[i])
       }
@@ -103,6 +91,7 @@
 </script>
 
 <style scoped lang="scss">
+
 ::v-deep .type-tabs-item{
   background:white;
   &.selected{
@@ -114,6 +103,26 @@
 }
 ::v-deep .interval-tabs-item{
   height:48px;
+}
+
+%item{
+  padding:8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.title{
+  @extend %item;
+}
+.record{
+  @extend %item;
+  background: white;
+}
+.notes{
+  margin-right: auto;
+  margin-left: 16px;
+  color:#999
 }
 </style>
 
